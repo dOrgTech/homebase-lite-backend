7384ecb4d5c734f454b303bbfb108e0e02f0f0ce
Manank Patni <manank321@gmail.com>
2022-10-30 02:18:09 +0530

    Get signature payload bytes from the request body
    Signed-off-by: Manank Patni <manank321@gmail.com>

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,23 +1,12 @@
-const { char2Bytes, verifySignature } = require("@taquito/utils");
+const { verifySignature } = require("@taquito/utils");
 
 const requireSignature = async (request, response, next) => {
-  const { signature, userAddress, publicKey } = request.body;
+  const { signature, userAddress, publicKey, payloadBytes } = request.body;
 
   if (!signature || !userAddress || !publicKey) {
     response.status(500).send("Invalid Signature Payload");
     return;
   }
-
-  const formattedInput = [
-    "Tezos Signed Message:",
-    // "http://localhost:3000",
-    // new Date().toISOString(),
-    `Tezos Homebase Lite - I am ${userAddress}`,
-  ].join(" ");
-
-  const bytes = char2Bytes(formattedInput);
-  const payloadBytes =
-    "05" + "0100" + char2Bytes(bytes.length.toString()) + bytes;
 
   const isVerified = verifySignature(payloadBytes, publicKey, signature);
   console.log("isVerified: ", isVerified);
