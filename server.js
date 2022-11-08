ec8cab7b771836cd5491987aa4f5f16a8f3e056e
Manank Patni <manank321@gmail.com>
2022-11-09 02:59:19 +0530

    Add cors
    Signed-off-by: Manank Patni <manank321@gmail.com>

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 app.use(require("./routes/daos"));
 app.use(require("./routes/polls"));
@@ -11,12 +15,11 @@ app.use(require("./routes/tokens"));
 app.use(require("./routes/choices"));
 // get driver connection
 const dbo = require("./db/conn");
- 
+
 app.listen(port, () => {
   // perform a database connection when server starts
   dbo.connectToServer(function (err) {
     if (err) console.error(err);
- 
   });
   console.log(`Server is running on port: ${port}`);
 });
