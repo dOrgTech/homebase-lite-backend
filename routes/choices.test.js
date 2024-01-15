9f37585b61068192ac254c9763abdac711beb7b5
Ben Walker <ben.walker18@gmail.com>
2024-01-15 15:40:21 -0800

    Flesh out swagger & refactor choices tests

--- routes/choices.test.js
+++ routes/choices.test.js
@@ -5,30 +5,30 @@ const choicesRoutes = require("../routes/choices");
 const app = express();
 app.use(express.json());
 app.use("/", choicesRoutes);
-const choiceId = 123;
+const id = 123;
 
 describe("Choices Routes", () => {
   it("should not find a choice with an invalid ID", async () => {
     await request(app)
-      .get(`/choices/${choiceId}/find`)
+      .get(`/choices/${id}/find`)
       .expect(400)
       .expect("Content-Type", /json/)
   });
   it("should not find votes for an nonexistent choice", async () => {
     await request(app)
-      .get(`/choices/${choiceId}/user_votes`)
+      .get(`/choices/${id}/user_votes`)
       .expect(400)
       .expect("Content-Type", /json/)
   });
   it("should not find choices for an nonexistent user", async () => {
     await request(app)
-      .get(`/choices/${choiceId}/user`)
+      .get(`/choices/${id}/user`)
       .expect(400)
       .expect("Content-Type", /json/)
   });
   it("should not find votes for a choice with an invalid ID", async () => {
     await request(app)
-      .get(`/choices/${choiceId}/votes`)
+      .get(`/choices/${id}/votes`)
       .expect(400)
       .expect("Content-Type", /json/)
   });
