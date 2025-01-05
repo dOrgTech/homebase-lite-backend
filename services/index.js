c8d90b404920972d47216e4178dbb22b33d4727f
Ashutosh Kumar <ashutosh@w3dev.email>
2025-01-05 16:26:55 +0000

    XSS Attacks Fixed

--- services/index.js
+++ services/index.js
@@ -18,8 +18,10 @@ const getTokenMetadata = async (contractAddress, network, tokenId) => {
 
   const response = await axios.get(url);
 
-  if (response.status !== 201) {
+  if (response.status > 299) {
     const errorId = nanoid()
+    const responseData = response?.data
+    console.log("getTokenMetadata", contractAddress, network, tokenId, responseData, response.status)
     throw new Error(`Failed to fetch proposals from BakingBad API: ${errorId}`);
   }
 
